45c2304001bd22300b12ed73b490523a2a59caf5
Smerlyn Eusebio <smerlyneusebio@GreenBowl.local>
2020-11-02 09:37:11 -0400

    [FIX] android platform '' npx react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res''

--- src/screens/MapViewScreen.js
+++ src/screens/MapViewScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet,Platform } from 'react-native';
 
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
@@ -7,21 +7,43 @@ import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import { CustomHeader } from '../components/organisms'
 
 function MapViewScreen(props) {
-    return (
-        <View>
-<CustomHeader name='Contacto' context={props} />
-
-            <MapView
-                style={styles.map}
-                initialRegion={{
-                    latitude: 18.735693,
-                    longitude: -70.162651,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421,
-                }}
-            />
-        </View>
-    )
+    if(Platform.OS === 'ios'){
+        
+        return (
+            <View>
+            <CustomHeader name='Encuentrate' context={props} />
+    
+                <MapView
+                    style={styles.map}
+                    initialRegion={{
+                        latitude: 18.735693,
+                        longitude: -70.162651,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421,
+                    }}
+                />
+            </View>
+        )
+    }
+    else if(Platform.OS === 'android'){
+        
+        return (
+            <View>
+            <CustomHeader name='Encuentranos' context={props} />
+    
+                <MapView
+                    provider={PROVIDER_GOOGLE}
+                    style={styles.map}
+                    initialRegion={{
+                        latitude: 18.735693,
+                        longitude: -70.162651,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421,
+                    }}
+                />
+            </View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
