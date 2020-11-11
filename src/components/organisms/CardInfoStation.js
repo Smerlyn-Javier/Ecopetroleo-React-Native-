9836d3a8f6f4665f7a83f154d44081d65855f4eb
Smerlyn Eusebio <smerlyneusebio@GreenBowl.local>
2020-11-11 10:16:08 -0400

    [ADD] direction map 15%

--- src/components/organisms/CardInfoStation.js
+++ src/components/organisms/CardInfoStation.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { Text, View, StyleSheet, Image, Linking, TouchableOpacity } from 'react-native'
 
-import MapView, { Marker } from 'react-native-maps';
+import MapView, { Marker,Polyline } from 'react-native-maps';
+import GetLocation from 'react-native-get-location'
+import MapViewDirections from 'react-native-maps-directions';
 
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
+const origin = { latitude: 18.735693, longitude: -70.162651 };
+const destination = { atitude: 18.735695, longitude: -70.162652 };
+const GOOGLE_MAPS_APIKEY = 'AIzaSyAZC6s8YMGHtnSoeM8p3-d_oVfiKOCe0pw';
 
 function CardInfoStation(props) {
     if (props.type === 'SERVICES') {
@@ -37,10 +42,26 @@ function CardInfoStation(props) {
         )
     }
     else if (props.type === 'MAP') {
+
+
+
+        // GetLocation.getCurrentPosition({
+        //     enableHighAccuracy: true,
+        //     timeout: 15000,
+        // })
+        // .then(location => {
+        //     console.log(location);
+        // })
+        // .catch(error => {
+        //     const { code, message } = error;
+        //     console.warn(code, message);
+        // })
+
+
         return (
 
             <View style={styles.mapContainer}>
-                <View style={{display:'flex',flexDirection:'row', marginBottom:25}}>
+                <View style={{ display: 'flex', flexDirection: 'row', marginBottom: 25 }}>
                     <Icon name="map-marker" size={25} color='#5c5c5c' />
                     <View style={styles.textmapContainer}>
                         <Text style={styles.mapTitle}>UBICACIÓN</Text>
@@ -53,6 +74,8 @@ function CardInfoStation(props) {
                         style={styles.mapIos}
                         zoomEnabled={false}
                         showsUserLocation={true}
+                        // maxZoomLevel={7}
+                        // minZoomLevel={9}
                         initialRegion={{
                             latitude: 18.735693,
                             longitude: -70.162651,
@@ -61,11 +84,32 @@ function CardInfoStation(props) {
                         }}
                     >
 
-                        <Marker
-                            coordinate={{ latitude: 18.735693, longitude: -70.162651 }}
-                            title='SOY YO'
-                            image={require('../../assets/images/resources/Marker-icon.png')}
+                        <Polyline
+                        geodesic={true}
+                        coordinates={[{ latitude: 18.5528159, longitude: -69.9279594 },{ latitude: 18.4861446, longitude: -69.894265 }]}
+                        strokeWidth={1}
                         />
+
+                        {/* <Marker
+                            coordinate={{ latitude: 18.5528159, longitude: -69.9279594 }}
+                            title='Ubicación Actual'
+                            image={require('../../assets/images/resources/Marker-icon-ios.png')}
+                        />
+
+                        <Marker
+                            coordinate={{ latitude: 18.4861446, longitude: -69.894265 }}
+                            title='Ubicación Actual'
+                            image={require('../../assets/images/resources/Marker-icon-ios.png')}
+                        />
+
+                        <MapViewDirections
+                            origin={{ latitude: parseFloat(18.5528159), longitude: parseFloat(-69.9279594) }}
+                            destination={{ atitude: parseFloat(18.4861446), longitude: parseFloat(-69.894265) }}
+
+                            apikey='AIzaSyAZC6s8YMGHtnSoeM8p3-d_oVfiKOCe0pw'
+                            strokeWidth={3}
+                            strokeColor='hotpink'
+                        /> */}
 
                     </MapView>
 
@@ -196,10 +240,9 @@ const styles = StyleSheet.create({
         color: '#787878',
         fontSize: 10,
         fontWeight: '400',
-        width: '90%'
+        width: '80%'
     },
     contentMap: {
-
         height: 300,
     },
     mapIos: {
